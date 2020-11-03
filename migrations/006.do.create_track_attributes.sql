e9d4464a65236d51b8c8122cd7548f65ce7c1949
Laura Sikora <lauraannsikora@gmail.com>
2020-11-03 16:34:34 -0400

    updated migrations

--- migrations/006.do.create_track_attributes.sql
+++ migrations/006.do.create_track_attributes.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS track_attributes;
 
 CREATE TABLE track_attributes (
+    author INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
     assigned_hike_id INTEGER REFERENCES hikes(id) ON DELETE CASCADE NOT NULL,
     track_id INTEGER REFERENCES trail_tracks(id) ON DELETE SET NULL,
     attribute TEXT NOT NULL  
