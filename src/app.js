91e38a192bec627f6c52d63b70613cbd51a41b93
Laura Sikora <lauraannsikora@gmail.com>
2020-11-19 11:23:20 -0600

    updated app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
-require('dotenv').config();
-const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const express = require('express');
 const { NODE_ENV } = require('./config');
 const app = express();
 const tracksRouter = require('./tracks/tracks-router');
@@ -10,15 +9,12 @@ const usersRouter =require('./users/users-router');
 const authRouter = require('./auth/auth-router');
 const hikesRouter = require('./hikes/hikes-router');
 const morganOption = (NODE_ENV === 'production')? 'tiny' : 'common';
-
 const {CLIENT_ORIGIN} = require('./config');
 
-app.use('Access-Control-Allow-Origin':'http://www.example.com');
-let cors = require('cors') app.use(cors())
-
+app.use(cors());
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+
 app.use(authRouter);
 app.use(usersRouter);
 app.use(tracksRouter);
@@ -27,6 +23,7 @@ app.use(hikesRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello, from Trail capstone!');
+    res.send('Hello, world!');
 });
 
 app.use(function errorHandler(error, req, res, next) {
@@ -39,5 +36,4 @@ app.use(function errorHandler(error, req, res, next) {
     }
     res.status(500).json(response);
 });
-
 module.exports = app;
