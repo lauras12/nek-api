70dbd0c868f29fd0264576d604e07dd9967b9414
Laura Sikora <lauraannsikora@gmail.com>
2020-11-19 16:12:22 -0600

    updated app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const usersRouter =require('./users/users-router');
 const authRouter = require('./auth/auth-router');
 const hikesRouter = require('./hikes/hikes-router');
 const morganOption = (NODE_ENV === 'production')? 'tiny' : 'common';
-const {CLIENT_ORIGIN} = require('./config');
+
 
 app.use(cors());
 app.use(morgan(morganOption));
